9b99580171146f6a081dab306eb697734d76f12d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-13 22:39:00 -0300

    Atualizando Populares

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,9 +1,26 @@
-import Titulo from "../../Titulo"
+import styled from "styled-components";
+import Titulo from "../../Titulo";
+import arrayFotos from "../../../fotosPopulares.json";
+import { useState } from "react";
+import ImagemPopulares from "./ImagemPopulares";
+
+const PopularesEstilizadas = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & img {
+    border-radius: 20px;
+  }
+`;
 
 const Populares = () => {
+  const [fotosPopulares, setFotosPopulares] = useState(arrayFotos);
   return (
-    <Titulo>Populares</Titulo>
-  )
-}
+    <PopularesEstilizadas>
+      <Titulo>Populares</Titulo>
+      <ImagemPopulares fotos={fotosPopulares} />
+    </PopularesEstilizadas>
+  );
+};
 
-export default Populares
+export default Populares;
