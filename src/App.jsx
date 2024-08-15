114ce1b9b58c8438d88ed3e7ee0a0cf675b44aef
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-14 22:39:32 -0300

    Atualizando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,67 @@
-import { styled } from "styled-components"
-import EstilosGlobais from "./componentes/EstilosGlobais"
-import Cabecalho from "./componentes/Cabecalho"
-import BarraLateral from "./componentes/BarraLateral"
-import Banner from "./componentes/Banner"
-import bannerBackground from './assets/banner.png'
-import Galeria from "./componentes/Galeria"
+import { styled } from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import bannerBackground from "./assets/banner.png";
+import Galeria from "./componentes/Galeria";
 
-import fotos from './fotos.json'
-import { useState } from "react"
-import ModalZoom from "./componentes/ModalZoom"
+import fotos from "./fotos.json";
+import { useEffect, useState } from "react";
+import ModalZoom from "./componentes/ModalZoom";
+import Rodape from "./componentes/Rodape";
 
 const FundoGradiente = styled.div`
-  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
   width: 100%;
   min-height: 100vh;
-`
+`;
 
 const AppContainer = styled.div`
   width: 1440px;
   margin: 0 auto;
   max-width: 100%;
-`
+`;
 
 const MainContainer = styled.main`
   display: flex;
   gap: 24px;
-`
+`;
 
 const ConteudoGaleria = styled.section`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-`
+  width: 1200px;
+`;
 
 const App = () => {
-  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
-  const [fotoSelecionada, setFotoSelecionada] = useState(null)
-  
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+  const [fotoSelecionada, setFotoSelecionada] = useState(null);
+
+  const [tag, setTag] = useState(0);
+  const [filtro, setFiltro] = useState("");
+
+  useEffect(() => {
+    const fotosFiltradas = fotos.filter((foto) => {
+      const filtroPorTag = !tag || foto.tagId === tag;
+      const filtroPorTitulo =
+        !filtro || foto.titulo.toLowerCase().includes(filtro.toLowerCase());
+      return filtroPorTag && filtroPorTitulo;
+    });
+    setFotosDaGaleria(fotosFiltradas);
+  }, [filtro, tag]);
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
-        <Cabecalho />
+        <Cabecalho filtro={filtro} setFiltro={setFiltro} />
         <MainContainer>
           <BarraLateral />
           <ConteudoGaleria>
@@ -49,19 +69,21 @@ const App = () => {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria 
-              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+            <Galeria
+              aoFotoSelecionada={(foto) => setFotoSelecionada(foto)}
               fotos={fotosDaGaleria}
+              setTag={setTag}
             />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
-      <ModalZoom 
+      <Rodape />
+      <ModalZoom
         foto={fotoSelecionada}
         aoFechar={() => setFotoSelecionada(null)}
       />
     </FundoGradiente>
-  )
-}
+  );
+};
 
-export default App
+export default App;
