40edc62100d90c12d130d9bde48ad78a07708f85
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-15 06:10:41 -0300

    Atualizando Populares

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -12,6 +12,19 @@ const PopularesEstilizadas = styled.div`
     border-radius: 20px;
   }
 `;
+const BotaoEstilizado = styled.button`
+  width: 70%;
+  border-radius: 10px;
+  padding: 20px 0;
+  color: #fff;
+  cursor: pointer;
+  font-size: 20px;
+  border: 2px solid transparent;
+  background-image: linear-gradient(180.61deg, #162E65 , #0A2B59),
+    radial-gradient(circle at top left, #C98CF1, #7B78E5);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+`
 
 const Populares = () => {
   const [fotosPopulares, setFotosPopulares] = useState(arrayFotos);
@@ -19,6 +32,7 @@ const Populares = () => {
     <PopularesEstilizadas>
       <Titulo>Populares</Titulo>
       <ImagemPopulares fotos={fotosPopulares} />
+      <BotaoEstilizado>Ver mais</BotaoEstilizado>
     </PopularesEstilizadas>
   );
 };
